64e0133fb6faf7daf9587527b0b6455904e33bc5
Suikei Wong <Suikei.Wong@anu.edu.au>
2020-07-25 19:40:58 +1000

    Update example-test.py

--- example-test.py
+++ example-test.py
@@ -2,15 +2,22 @@
 import torchvision.transforms as transforms
 import torch
 from PIL import Image
+from model import MobileNet
 
 
 def main():
+	# Load best model
+	checkpoint_path = './model_checkpoint.pth'
+	model = MobileNet().to('cuda')
+	checkpoint = torch.load(checkpoint_path)
+	print("model load successfully.")
+	
 	transform = transforms.Compose(
 	        [transforms.Resize(224),
 	         transforms.ToTensor(),
 	         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 	         
-	image_set = Image.open('/content/test.jpg')
+	image_set = Image.open('./test.jpg')
 
 	image_tensor = transform(image_set)
 	image = torch.unsqueeze(image_tensor, 0)
